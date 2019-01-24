a84b1e3c849a5617bb49d07a33dde48d08c7965c
明涛涛 <mingtaotao@kjtpay.com.cn>
2019-01-24 17:26:12 +0800

    漏了点东西redis。

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(HomePic)
+class HomePicAdmin(admin.ModelAdmin):
+    list_display = ['name', 'ph', 'pic_num', 'show']
+    def image_tag(self):
+        url = self.urls
+        return u'<img src="%s" />' % url
+    image_tag.short_description = 'Image'
+    image_tag.allow_tags = True
+    pass
--- homepage/models.py
+++ homepage/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+'''
 class Order(models):
     ip = models.GenericIPAddressField()
 
@@ -19,3 +19,21 @@ class Comment(models):
 
 class Picture(models):
     pic_num = models.ImageField()
+'''
+
+
+class HomePic(models.Model):
+    ph = models.ImageField(u'图片', upload_to='uploadImages')
+    name = models.CharField(u'名称', max_length=256)
+    url = models.CharField(u'链接', max_length=256)
+    pic_num = models.IntegerField(u'排序')
+    show = models.BooleanField(u'显示')
+
+    class Meta:
+        ordering = ['pic_num']
+        verbose_name = "首页图片"
+        verbose_name_plural = "首页图片"
+
+    def __str__(self):
+        return self.name
+
